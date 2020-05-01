9b5c319ef578ff20d489c8fad5f8227ec811b9a3
Robin Geelen <robin-geelen@hotmail.com>
2020-05-01 16:43:16 +0200

    Change video feed

--- include/general/params.h
+++ include/general/params.h
@@ -59,8 +59,8 @@ typedef void	(*writeOutput)(uint8_t *buffer, size_t size);
 #define BITS 32
 #define SIZE 8
 #define PIPE_BUFFER_SIZE	65535
-#define MAX_PACKET_SIZE		1700
-#define DECODER_BUFFER_SIZE 1500	/* Should be large enough for video packet */
+#define MAX_PACKET_SIZE		17000
+#define DECODER_BUFFER_SIZE 15000	/* Should be large enough for video packet */
 #define MAKE_BER			0
 #define FRAC_BER			0.0007
 
