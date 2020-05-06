723e51f2382640213dd5cf2260851896ed3ce61d
Robin Geelen <robin-geelen@hotmail.com>
2020-05-06 09:49:58 +0200

    Video feed reliable

--- include/general/params.h
+++ include/general/params.h
@@ -48,7 +48,7 @@ struct pipe {
 #endif
 
 /* Reliable/unreliable video feed */
-#define RELIABLE_FEED   0
+#define RELIABLE_FEED   1
 #define PACKET_INTERVAL	500			/* In microseconds */
 
 /* Type definitions */
