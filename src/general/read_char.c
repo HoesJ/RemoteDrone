1346019dc5e43b3bfc19c19271a5a33ce2c3fe90
Robin Geelen <robin-geelen@hotmail.com>
2020-05-16 13:50:45 +0200

    Change readChar function

--- src/general/read_char.c
+++ src/general/read_char.c
@@ -1,10 +1,5 @@
 #include "./../../include/general/read_char.h"
 
 uint8_t readChar() {
-    return
-    #if UNIX
-        getchar();
-    #else
-        getch();
-    #endif
+    return getchar();
 }
