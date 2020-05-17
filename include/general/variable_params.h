29defaf1528bbe445f37dd268a2a6a14e7656e62
Robin Geelen <robin-geelen@hotmail.com>
2020-05-17 17:41:34 +0200

    Add defines to variables params

--- include/general/variable_params.h
+++ include/general/variable_params.h
@@ -4,8 +4,12 @@
 #define FEED_INPUT  "./feed.jpg"
 #define FEED_OUTPUT "./rec_feed.jpg"
 
+#define DEFAULT_IP
+#define SIZE_32
+#define LIVE_FEED
+
 #ifdef  DEFAULT_IP
-#define DEST_IP     "127.0.0.1" /* "94.226.244.66" */
+#define DEST_IP     "127.0.0.1" /* "84.194.50.109" */
 #endif
 
 #endif
